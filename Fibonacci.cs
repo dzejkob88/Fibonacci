b2a5401c6297dea839b21e46cfea7fe3968fe301
Jakub Noras <jakubnoras@gmail.com>
2020-06-03 19:18:52 +0200

    Add project files.

---
+++ Fibonacci.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Fibonacci
+    {
+        public static int fibo(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+            return fibo(n - 2) + fibo(n - 1);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj ilość argumentów: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Fibonacci.fibo(a));
+
+            Console.ReadKey();
+        }
+    }
+}
